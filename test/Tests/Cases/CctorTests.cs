ca64a819026f313a9c12db466536d8cd3c7353e8
ihourglass <inversionhourglass@outlook.com>
2024-10-20 02:21:50 +0800

    Add cctor test cases.

--- test/Tests/Cases/CctorTests.cs
+++ test/Tests/Cases/CctorTests.cs
@@ -1,4 +1,5 @@
 ﻿using Mono.Cecil;
+using Mono.Cecil.Rocks;
 using System.Linq;
 using Xunit;
 
@@ -10,12 +11,24 @@ namespace Tests.Cases
         [ClassData(typeof(AssemblyData))]
         public void AnyCctorTest(AssemblyDefinition assemblyDef, bool compositeAccessibility)
         {
-            var pattern = "cctor(*(..))";
+            var pattern = "cctor(*)";
             var patternMatches = assemblyDef.FindMethods(pattern, compositeAccessibility).ToArray();
             var lambdaMatches = assemblyDef.FindMethods(md => md.IsConstructor && md.IsStatic).ToArray();
 
             Assert.NotEmpty(patternMatches);
             Assert.Equal(lambdaMatches, patternMatches);
+        }
+
+        [Theory]
+        [ClassData(typeof(AssemblyData))]
+        public void SpecificDeclaringTest(AssemblyDefinition assemblyDef, bool compositeAccessibility)
+        {
+            var expectedMethod = assemblyDef.RandomType(static td => !td.HasGenericParameters && !td.IsNested && td.GetStaticConstructor() != null).GetStaticConstructor();
+            var pattern = $"cctor({expectedMethod.DeclaringType.FullName})";
+            var patternMatches = assemblyDef.FindMethods(pattern, compositeAccessibility).ToArray();
+
+            Assert.Single(patternMatches);
+            Assert.Equal(expectedMethod, patternMatches[0]);
         }
     }
 }
